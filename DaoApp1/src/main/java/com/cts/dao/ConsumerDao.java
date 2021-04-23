cc377264fde472feb393e35222d9dfdd9af31601
dhanush013 <40061153+dhanush013@users.noreply.github.com>
2021-04-23 15:22:01 +0530

    23/04/21-1521

--- DaoApp1/src/main/java/com/cts/dao/ConsumerDao.java
+++ DaoApp1/src/main/java/com/cts/dao/ConsumerDao.java
@@ -1,5 +1,48 @@
 package com.cts.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cts.util.DbConnectionManager;
+import com.cts.model.Consumer;
+
 public class ConsumerDao {
+	
+	private Connection connection;
+	private Statement statement;
+	private PreparedStatement preparedStatement;
+	private ResultSet resultSet;
+	private List<Consumer> consumerList;
+	private Consumer consumer;
+	
+	public ConsumerDao() throws SQLException {
+		connection= DbConnectionManager.myConnection();
+		
+	}
+	
+	public List<Consumer> getAllConsumer() throws SQLException {
+		statement=connection.createStatement();
+		resultSet= statement.executeQuery("select * from consumer");
+		consumerList=new ArrayList<Consumer>();
+		
+		while(resultSet.next()) {
+			consumer=null;
+			consumer= new Consumer();
+			consumer.setId(resultSet.getInt(1));
+			consumer.setName(resultSet.getString(2));
+			consumer.setSalary(resultSet.getFloat(3));
+			consumer.setDob(resultSet.getDate(4));
+			
+			
+			
+		}
+		return consumerList
+	}
+	
 
 }
--- DaoApp1/src/main/java/com/cts/util/DbConnectionManager.java
+++ DaoApp1/src/main/java/com/cts/util/DbConnectionManager.java
@@ -1,5 +1,31 @@
 package com.cts.util;
 
-public class DbConnectionManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DbConnectionManager {
+	
+	private DbConnectionManager() {};
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}
+	}
+	
+	static Connection connection;
+	
+	public static Connection myConnection() throws SQLException {
+		
+		if(connection == null) {
+			connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/dbaj06","root","mysql");
+			
+		}
+		return connection;
+		
+	}
+	
+	
 }
