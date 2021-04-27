76845f2abb0f477e0e069feba90997e729360ecc
dhanush013 <40061153+dhanush013@users.noreply.github.com>
2021-04-27 10:16:34 +0530

    ..

--- SpringJdbcPrj1/src/main/java/cts/com/dao/CustomerDao.java
+++ SpringJdbcPrj1/src/main/java/cts/com/dao/CustomerDao.java
@@ -1,5 +1,17 @@
 package cts.com.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class CustomerDao {
+	
+	private JdbcTemplate jdbcTemplate;
+
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
 }
