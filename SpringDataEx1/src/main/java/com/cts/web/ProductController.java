f14090be9c197271c67845b8db43fe4b51ecbe6a
dhanush013 <40061153+dhanush013@users.noreply.github.com>
2021-05-26 14:48:45 +0530

    ..

--- SpringDataEx1/src/main/java/com/cts/web/ProductController.java
+++ SpringDataEx1/src/main/java/com/cts/web/ProductController.java
@@ -1,5 +1,27 @@
 package com.cts.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cts.model.Product;
+import com.cts.service.ProductService;
+
+@Controller
+@RequestMapping("/product")
 public class ProductController {
+	
+	@Autowired
+	ProductService service;
+	
+	@RequestMapping("/all")
+	@ResponseBody
+	public List<Product> allProduct(){
+		return service.getAllProduct();
+	}
+	
 
 }
