a5f6ef06ceb307cc8a27239a095258a302587c11
dhanush013 <40061153+dhanush013@users.noreply.github.com>
2021-05-26 13:45:13 +0530

    ..

--- JpaDemo1/src/main/java/com/cts/EmployeeMain.java
+++ JpaDemo1/src/main/java/com/cts/EmployeeMain.java
@@ -1,11 +1,16 @@
 package com.cts;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import com.cts.model.Employee;
+
 
 public class EmployeeMain {
 	
@@ -15,20 +20,44 @@ public class EmployeeMain {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
 		Employee emp1 = new Employee();
-		emp1.setId(10001);
-		emp1.setName("Mohan Kumar");
-		emp1.setSalary(12000);
+		emp1.setId(10005);
+		emp1.setName("E Kumar");
+		emp1.setSalary(18000);
+		emp1.setDob(LocalDate.of(1995, 12, 25));
 		
 		EntityTransaction transaction = entityManager.getTransaction();
 		
 		transaction.begin();
 		
 		entityManager.persist(emp1);
-		
+
 		transaction.commit();
 		
 		System.out.println("Records Added");
 		
+		Employee find = entityManager.find(Employee.class, 10000);
+		if(find!=null) {
+			System.out.println("Record Found");
+			System.out.println(find.getId()+" "+find.getName()+" "+find.getSalary());
+			
+		}else
+			System.out.println("Record Not Found");
+		
+		transaction.begin();
+		entityManager.remove(find);
+		transaction.commit();
+		
+		System.out.println("record deleted");
+		
+		Query createQuery = entityManager.createQuery("from Employee");
+		
+		List<Employee> resultList = createQuery.getResultList();
+		
+		for(Employee e:resultList) 
+			System.out.println(e.getId()+" "+e.getName()+" "+e.getSalary());
+		
+		entityManager.close();
+		entityManagerFactory.close();
 	
 	}
 
--- JpaDemo1/src/main/java/com/cts/model/Employee.java
+++ JpaDemo1/src/main/java/com/cts/model/Employee.java
@@ -1,33 +1,60 @@
 package com.cts.model;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "bankEmployee")
 public class Employee {
 	
 	@Id
+	@Column(name = "empId")
 	private int id;
+	
+	@Column(name = "empName", length = 20 ,nullable = false)
 	private String name;
+	
+	@Column(name = "empSalary", nullable = false )
 	private float salary;
 	
+	@Column(name = "empDob", nullable = false )
+	private LocalDate dob;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public float getSalary() {
 		return salary;
 	}
+
 	public void setSalary(float salary) {
 		this.salary = salary;
 	}
 
+	public LocalDate getDob() {
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+	
+	
 }
