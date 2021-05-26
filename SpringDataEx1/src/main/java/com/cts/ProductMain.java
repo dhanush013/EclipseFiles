a880ed5ba97ecc616003c7ecbc3e6939b3943787
dhanush013 <40061153+dhanush013@users.noreply.github.com>
2021-05-26 14:51:46 +0530

    ..

---
+++ SpringDataEx1/src/main/java/com/cts/ProductMain.java
@@ -0,0 +1,11 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+
+public class ProductMain {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMain.class, args);
+	}
+
+}
